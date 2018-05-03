5eddf1459a0325a40ad50fff2e3c4d8589e01c24
SondreHerr <sondreherr@gmail.com>
2018-05-03 14:22:55 +0200

    personeller mqtt

---
+++ Python/mqtt_personteller_snippet.py
@@ -0,0 +1,23 @@
+import paho.mqtt.client as mqtt
+
+
+# SETTING UP A MQTT CLIENT
+client = mqtt.Client("python_publisher")
+
+# SETTING USERNAME AND PASSWORD FOR MQTT
+client.username_pw_set(username="pi", password="raspberry")
+
+# SETTING SERVER UP TO PUBLISH TO 192.168.1.41, THIS NEEDS TO BE YOUR PIS IP ADDRESS
+client.connect("192.168.1.41", 1883)
+client.loop_start()
+
+try:
+
+    while True:
+        # GENERATING TOPIC AND PUBLISHING
+        #
+        client.publish("personteller/antall", total + " personer i rommet")
+
+except KeyboardInterrupt:
+    print("interrupted")
+    client.loop_stop()
